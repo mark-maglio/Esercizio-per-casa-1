04589394e4a3735be6adb56de465740debf47caf
mark maglio <markmaglio25@gmail.com>
2020-03-02 20:44:07 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio per casa 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Esercizio_per_casa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci un numero!");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine($"il numero {n} è PARI");
+                    for (int i = 0; i <= n; i++)
+                    {
+                        Console.WriteLine(i.ToString());
+                    }
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"il numero {n} è DISPARI");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("il numero inserito è minore di 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
